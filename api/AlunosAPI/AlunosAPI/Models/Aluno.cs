08686eb707769c5603103ef775f6ffdc824a5b67
FernandoRibeiro97 <fernandophanton@hotmail.com>
2026-06-16 13:03:11 -0300

    Estrutura inicial

---
+++ api/AlunosAPI/AlunosAPI/Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosAPI.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage = "O tamanho máximo para {0} é de 80 caracteres!")]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "O tamanho máximo para {0} é de 100 caracteres!")]
+        public string Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
